d63c68d0c40f64f9802002e737a06e3d94a879ce
Marcus Read <marcusaread@gmail.com>
2022-06-24 12:27:03 +0100

    Move build to pyproject.py and use setuptools-scm
    Moves meta from setup.cfg to pyproject.toml.
    
    Implements setuptools-scm in build to version builds and create
    manifests.

---
+++ setup.py
@@ -0,0 +1,8 @@
+# This file is currently (2022-06) required in order for pip to be able to create
+# editable installs when build meta is included to pyproject.toml. Likely that at
+# some future point this will no longer be required and file can be removed.
+# Reference: https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+
+from setuptools import setup
+
+setup()
