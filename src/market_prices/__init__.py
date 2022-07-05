bec8ca76068b723e5d6d89d7c9b218407a81c392
Marcus Read <marcusaread@gmail.com>
2022-07-05 17:19:39 +0100

    Update resolving of __init__.__version__

--- src/market_prices/__init__.py
+++ src/market_prices/__init__.py
@@ -7,7 +7,26 @@ __all__ = [PricesYahoo, get_exchange_info]
 
 __copyright__ = "Copyright (c) 2022 Marcus Read"
 
+
+# Resolve version
+__version__ = None
+
 from importlib.metadata import version
 
-__version__ = version("market_prices")
+try:
+    # get version from installed package
+    __version__ = version("market_prices")
+except ImportError:
+    pass
+
+if __version__ is None:
+    try:
+        # if package not installed, get version as set when package built
+        from ._version import version
+    except Exception:
+        # If package not installed and not built, leave __version__ as None
+        pass
+    else:
+        __version__ = version
+
 del version
